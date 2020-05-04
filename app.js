a5f8a673b7c22c34ad1dff3830fa65d54e76b762
Lydia Gregory <ljg2gb@virginia.edu>
2020-05-04 17:14:13 -0600

    changes not to be saved

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
 
 const searchParams = new URLSearchParams(window.location.search);
 const search = searchParams.get('search');
+let usersURL = 'http://localhost:3000/users'
+if (search) {
+    usersURL = `http://localhost:3000/users?search=${search}`
+}
 console.log(search)
 
 fetch('http://localhost:3000/users')
     .then(resp => resp.json())
-    .then(handleUsers)
+    .then(addDropDownOptions)
 
-function handleUsers(users) {
-    const foundUser = users.find( user => {
-        // console.log(user.name)
-        findUser(user)
+// function handleUsers(users) {
+//     const foundUser = users.find( user => {
+//         // console.log(user.name)
+//         findUser(user)
+//     })
+//     console.log(foundUser)
+// }
+
+// function findUser(user) {
+//     if (user.name == search) 
+//     {return user}
+// }
+
+const $select = document.querySelector('#users')
+function addDropDownOptions(users) {
+    users.forEach(user => {
+        const $option = document.createElement('option')
+        $option.value = user.id
+        $option.innerText = user.name
+        $select.append($option)
     })
-    console.log(foundUser)
+
 }
 
-function findUser(user) {
-    if (user.name == search) 
-    {return user}
-}
 
